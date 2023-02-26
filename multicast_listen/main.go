a8ef562394d55eb7c92a9dad62634a29d635b310
hyson007 <hyson007@gmail.com>
2023-02-26 23:36:34 +0800

    add cobra on both client & server

--- multicast_listen/main.go
+++ multicast_listen/main.go
@@ -88,88 +88,32 @@ func receivePackets(cmd *cobra.Command, args []string) {
 	}
 	defer conn.Close()
 
-	//	file, err := conn.File()
-	//	if err!= nil {
-	//		log.Fatal("unable to get fd", err)
-	//	}
-	//	fd := file.Fd()
-	//	log.Println(fd)
-	// Set the IP_FREEBIND socket option to allow binding to an address that is already in use
-	//	err = syscall.SetsockoptInt(int(fd), syscall.SOL_SOCKET, syscall.IP_FREEBIND, 1)
-	//	if err != nil {
-	//		fmt.Println("error enabling IP_FREEBIND")
-	//		return
-	//	}
-
-	//	err = unix.SetsockoptInt(int(fd), unix.SOL_SOCKET, unix.SO_REUSEPORT, 1)
-	//	if err != nil {
-	//		fmt.Printf("Error enabling SO_REUSEADDR option: %v\n", err)
-	//		return
-	//	}
-
-	//	p := ipv4.NewPacketConn(conn)
-	//	fmt.Println(addr)
-	//	if err := p.JoinGroup(interFace, addr); err != nil {
-	//		log.Fatal("unable to join group, err: ", err)
-	//		return
-	//	}
-
-	// // test
-	// if loop, err := p.MulticastLoopback(); err == nil {
-	// 	log.Printf("MulticastLoopback status:%v\n", loop)
-	// 	if !loop {
-	// 		if err := p.SetMulticastLoopback(true); err != nil {
-	// 			log.Printf("SetMulticastLoopback error:%v\n", err)
-	// 		}
-	// 	}
-	// }
-
-	//	if err := p.SetControlMessage(ipv4.FlagDst, true); err != nil {
-	//		log.Fatal("unable to set dst-mode on accept:", err)
-	//	}
-
-	//	fmt.Println("hit")
 	buf := make([]byte, 1500)
 	fmt.Println("listening on interface:", "group:", address, "port:", port, "on interface:", interFace.Name)
 	for {
 		n, _, err := conn.ReadFromUDP(buf)
+		_ = n
 		if err != nil {
-			fmt.Println("Error receiving data:", err)
+			log.Print(".")
 			continue
 		}
-		fmt.Println("Received data:", string(buf[:n]))
-		//n, cm, src, err := p.ReadFrom(b)
-		//		if err != nil {
-		//			log.Fatal("unable to read from packet, err: ", err)
-		//		}
-		//		log.Println("hit")
-		//		if cm.Dst.IsMulticast() {
-		//			if cm.Dst.Equal(net.IP(address)) {
-		//				// joined group, do something
-		//				if enableTimeDiff {
-		//					senderNano, err := strconv.Atoi(string(b[:n]))
-		//					if err != nil {
-		//						log.Println("error converting sender time to Nano, please enable -time-diff on sender")
-		//						continue
-		//					}
-		//					diffInNano := time.Now().UnixNano() - int64(senderNano)
-		//					log.Println("diffInMicroSeconds: ", diffInNano/1000)
-		//					continue
-		//				}
-		//
-		//				if printPayload {
-		//					log.Println("received multicast packet from: ", src, "data: ", string(b[:n]))
-		//				} else {
-		//					log.Print(".")
-		//				}
-		//
-		///			} else {
-		//				// unknown group, discard
-		//				continue
-		//			}
-		//		}
-		//	}
 
+		// if enableTimeDiff {
+		// 	senderNano, err := strconv.Atoi(string(buf[:n]))
+		// 	if err != nil {
+		// 		log.Println("error converting sender time to Nano, please enable -time-diff on sender")
+		// 		continue
+		// 	}
+		// 	diffInNano := time.Now().UnixNano() - int64(senderNano)
+		// 	log.Println("diffInMicroSeconds: ", diffInNano/1000)
+		// 	continue
+		// }
+
+		// if printPayload {
+		// 	log.Println("received multicast packet from: ", src, "data: ", string(buf[:n]))
+		// } else {
+		// 	log.Print(".")
+		// }
 	}
 }
 
